f5e4cbf2fccd928f3b0b30cca6f8b46e551ea5f2
lancercd <1311282756@qq.com>
2021-10-25 12:01:11 +0800

    src file init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import {Button} from "antd";
 
 class App extends Component {
     render() {
         return (
             <div>
                 app
+                <Button type="primary">btn</Button>
             </div>
         );
     }

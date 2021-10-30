ff60e1afbf6bcd9701497b96082503f556fbf1ed
lancercd <1311282756@qq.com>
2021-10-30 14:29:23 +0800

    添加 react-redux Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React, {Component, Fragment} from 'react';
 import Index from "./pages/Index";
+import {Provider} from "react-redux";
+
+import store from "./store";
 
 class App extends Component {
     render() {
         return (
             <Fragment>
-                <Index />
+                <Provider store={store} >
+                    <Index />
+                </Provider>
             </Fragment>
         );
     }

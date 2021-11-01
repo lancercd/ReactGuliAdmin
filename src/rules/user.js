e596e881f8ad05007e076bd071b56d0f00cd4fa1
lancercd <1311282756@qq.com>
2021-11-01 14:57:33 +0800

    用户名规则校验修改

--- src/rules/user.js
+++ src/rules/user.js
@@ -4,7 +4,7 @@
  */
 export const usernameRules = [
     { required: true, message: 'Please input your Username!'},
-    {min: 4, message: "用户名至少4位!"},
+    {min: 3, message: "用户名至少3位!"},
     {max: 12, message: "用户名最多12位!"},
     {pattern: /^[a-zA-Z0-9_]+$/, message: "用户名必须由英文、数字、下划线购成!"},
 ];

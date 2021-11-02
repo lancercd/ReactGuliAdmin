4ada6c9ea040b95906dd08aad80d92a8542427e9
lancercd <1311282756@qq.com>
2021-11-02 23:31:15 +0800

    menu 配置文件修改

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -13,64 +13,67 @@ import {
 const menuList = [
     {
         title: "home",
+        key: "home",
         path: "/home",
         icon: <MailOutlined />
     },
     {
         title: "admin",
+        key: "admin",
         path: "/admin",
         icon: <CalendarOutlined />
     },
     {
         title: "user",
+        key: "user",
         icon: <AppstoreOutlined />,
         children: [
             {
-                title: "user",
-                path: "/user",
+                title: "list",
+                key: "list",
+                path: "/user/list",
                 icon: <CalendarOutlined />
             },
             {
                 title: "Option 4",
+                key: "Option",
                 children: [
                     {
-                        title: "Option 5",
-                        path: "/Option_5",
+                        title: "5",
+                        key: "5",
+                        path: "/user/Option/5",
                     },
                     {
-                        title: "Option 6",
-                        path: "/Option_6",
+                        title: "6",
+                        key: "6",
+                        path: "/user/Option/6",
                     }
                 ]
             }
         ]
     },
     {
-        title: "Navigation_Three",
+        title: "category",
+        key: "category",
+        path: "/category",
         icon: <SettingOutlined />,
-        children: [
-            {
-                title: "Option 7",
-                path: "/Option_7"
-            },
-            {
-                title: "Option 8",
-                path: "/Option_8"
-            },
-            {
-                title: "Option 9",
-                path: "/Option_9"
-            },
-        ]
     },
     {
-        title: "category",
-        path: "/category",
-        icon: <LinkOutlined />,
+        title: "product",
+        key: "product",
+        path: "/product",
+        icon: <SettingOutlined />,
+    },
+    {
+        title: "role",
+        key: "role",
+        path: "/role",
+        icon: <SettingOutlined />,
     },
     {
         title: "ant.design",
-        path: "/ant/design",
+        key: "design",
+        path: "/design",
         icon: <LinkOutlined />,
     }
 ]

adfad8b23cf3bed4ec9c90c799051dab7a6ba4ee
lancercd <1311282756@qq.com>
2021-11-04 23:53:25 +0800

    添加category api

--- src/api/category.js
+++ src/api/category.js
@@ -6,3 +6,29 @@ export function categoryListApi() {
         method: "post"
     });
 }
+
+/**
+ * 添加
+ * @param data {name}
+ * @returns {Promise}
+ */
+export function addCategoryApi(data) {
+    return request({
+        url: "/manage/category/add",
+        method: "post",
+        data
+    })
+}
+
+/**
+ * 修改
+ * @param data {id, name}
+ * @returns {Promise}
+ */
+export function modifyCategoryApi(data) {
+    return request({
+        url: "/manage/category/modify",
+        method: "post",
+        data
+    })
+}

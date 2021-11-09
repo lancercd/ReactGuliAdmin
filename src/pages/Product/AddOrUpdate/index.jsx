6f628ddfc818641b659ae539a6b2ccb926b594b2
lancercd <1311282756@qq.com>
2021-11-09 21:05:35 +0800

    商品详情 添加修改商品页

---
+++ src/pages/Product/AddOrUpdate/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AddOrUpdate extends Component {
+    render() {
+        return (
+            <div>
+                <h3>addOrUpdate</h3>
+            </div>
+        );
+    }
+}
+
+export default AddOrUpdate;
---
+++ src/pages/Product/Detail/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Detail extends Component {
+    render() {
+        return (
+            <div>
+                <h3>detail</h3>
+            </div>
+        );
+    }
+}
+
+export default Detail;

54045c85280fd519ae13a236c00a57fd8f56a1aa
lancercd <1311282756@qq.com>
2021-11-11 23:05:27 +0800

    商品详情中 校验商品id  id不正确则跳转到商品列表页

--- src/pages/Product/Detail/index.jsx
+++ src/pages/Product/Detail/index.jsx
@@ -1,18 +1,53 @@
 import React, {Component} from 'react';
+import {Button, Card, message} from "antd";
+import {withRouter as WithRouter} from "react-router-dom";
+import {ArrowLeftOutlined} from "@ant-design/icons";
 
+
+import "./index.less";
+import {idRegExp} from "../../../rules/product";
+
+
+
+@WithRouter
 class Detail extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
+
+        this.id = props.match.params.id;
+
+        // 判断是否为数字  非数字跳转到商品列表
+        this.isValidate = idRegExp.test(this.id);
+        if(!this.isValidate) {
+            message.warn("商品id不合法!");
+            this.props.history.replace("/product/goods");
+        }
+    }
+
+    renderTitle() {
+        return (
+            <Button type="link" onClick={() => {this.props.history.push("/product/goods", {search:{type: "description", key: "手"}});}}>
+                <ArrowLeftOutlined />返回商品详情
+            </Button>
+        );
+    }
+
+    componentDidMount() {
+        if(!this.isValidate) return;
+        console.log("Detail: DidMount");
     }
 
 
     render() {
+        if(!this.isValidate) return null;
+        console.log("Detail: render");
+
         return (
-            <div>
-                <h3>detail</h3>
-                id:{this.props.match.params.id}
-            </div>
+            <Card title={this.renderTitle()}>
+                <div>111</div>
+                <div>111</div>
+                <div>111</div>
+            </Card>
         );
     }
 }

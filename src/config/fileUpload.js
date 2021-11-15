842bd28de2dd09d03e37b0b9b209bc86f99fee97
lancercd <1311282756@qq.com>
2021-11-15 23:50:43 +0800

    文件上传的简单配置

---
+++ src/config/fileUpload.js
@@ -0,0 +1,11 @@
+/**
+ * 上传图片地址
+ * @type {string}
+ */
+export const uploadImageUrl = "http://127.0.0.1:9527/api/upload/image";
+
+/**
+ * 图片回显地址前缀
+ * @type {string}
+ */
+export const imageUrlPrefix = "http://127.0.0.1:9527";
